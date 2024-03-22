c6ebbdeecab200a180ddd3cceeb8006ac7fe6e94
root <root@digitalkreatifsolusindo.cc>
2024-03-22 23:40:47 +0700

    update aje deh

---
+++ components/HealthyFriend/ChatButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="togglePopup" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full shadow-md">
+    <font-awesome-icon icon="fas fa-comment" />
+    Live Chat
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    togglePopup() {
+      this.$emit('toggle-popup');
+    }
+  }
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		<h1>
+			About page
+		</h1>
+	</div>
+</template>
