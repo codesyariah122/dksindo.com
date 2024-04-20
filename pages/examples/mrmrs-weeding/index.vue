9da67c5529f3ce7b451a9d6ea85e9425b800bb82
codesyariah122 <pujiermanto@gmail.com>
2024-04-21 00:42:41 +0700

    update puji lagi

--- pages/examples/mrmrs-weeding/index.vue
+++ pages/examples/mrmrs-weeding/index.vue
@@ -1,10 +1,6 @@
 <template>
 	<div>
 		<client-only>
-			<div v-show="loading" id="spinner" class="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
-				<div class="spinner-grow text-primary" role="status"></div>
-			</div>
-
 			<mrms-weeding-navigation />
 
 			<mrms-weeding-carousel />
