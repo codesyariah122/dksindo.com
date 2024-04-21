8c803d070a12fa2f31f1f0e8507047d452e5e1b6
codesyariah122 <pujiermanto@gmail.com>
2024-04-21 07:15:26 +0700

    update aje

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,21 @@
 <template>
 	<div>
-		<client-only>
-			<dksindo-navigation />
-			<dksindo-hero />
-			<ContentQuery
-			path="/"
-			:only="['title', 'description', 'headline', 'excerpt', 'date', 'tags', '_path', 'src']"
-			:sort="{
-				date: 1
-			}"
-			:limit="blogCountLimit"
-			v-slot="{ data }"
-			>
-			<dksindo-products :data="data"/>
-		</ContentQuery>
-		<dksindo-portfolio />
-		<dksindo-contact :teams="teams"/>
-		<dksindo-footer />
-	</client-only>
+		<dksindo-navigation />
+		<dksindo-hero />
+		<ContentQuery
+		path="/"
+		:only="['title', 'description', 'headline', 'excerpt', 'date', 'tags', '_path', 'src']"
+		:sort="{
+			date: 1
+		}"
+		:limit="blogCountLimit"
+		v-slot="{ data }"
+		>
+		<dksindo-products :data="data"/>
+	</ContentQuery>
+	<dksindo-portfolio />
+	<dksindo-contact :teams="teams"/>
+	<dksindo-footer />
 </div>
 </template>
 
